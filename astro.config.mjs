447030b90886e88ec4f0f8b1fc687e8a719815d4
Juan Miguel Guerrero <juanmiguel@existo.es>
2022-12-30 09:03:43 +0100

    Init commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  experimental: {
+    prerender: true
+  },
+  output: "server",
+  adapter: netlify()
+});
